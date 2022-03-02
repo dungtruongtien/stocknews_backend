53e23b55da5ddafdc1a5ab4c88152dd1040c1dc1
dungdev <dung.truong@kompa.ai>
2022-03-02 15:26:23 +0700

    update code

--- src/services/stock.service.ts
+++ src/services/stock.service.ts
@@ -10,27 +10,10 @@ export default class StockService {
     const { limit, page } = filter;
     // const skip = (page - 1) * limit;
     const total = await this.PersonalStockModel.countDocuments({ action: 'buy' });
-    // const stocks = await this.PersonalStockModel.find({ action: 'buy' })
-    //   .sort({ createdAt: -1 })
-    //   .skip(skip)
-    //   .limit(limit);
-    // const listTradingKey = stocks.map((stock: any) => stock.tradingKey);
-    // const stockSoldInfo = await this.PersonalStockModel.find({ tradingKey: { $in: listTradingKey } })
-    //   .sort({ createdAt: -1 });
-    // const stockInfo = stocks.map((stock: any) => {
-    //   let { status } = stock;
-    //   const latestStockInfo = stockSoldInfo.find((st: any) => st.tradingKey === stock.tradingKey);
-    //   if (latestStockInfo) {
-    //     status = latestStockInfo.status;
-    //   }
-    //   return {
-    //     ...stock.toJSON(),
-    //     status
-    //   };
-    // });
     const stocks = await this.PersonalStockModel.aggregate(
       [
         { $group: { _id: '$tradingKey', stocks: { $last: '$$ROOT' } } },
+        { $match: { 'stocks.status': 'hold' } },
         { $sort: { 'stocks.createdAt': -1 } },
         { $skip: (page - 1) * limit },
         { $limit: limit }
